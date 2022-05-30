97508264b82067f158f3b5224f17ab350de76d66
ogoras <skus30@wp.pl>
2022-05-30 16:16:29 +0200

    program parameters

--- PhongCylinder/index.js
+++ PhongCylinder/index.js
@@ -4,6 +4,16 @@ let canvas = document.getElementById("canvas");
 
 drawer.initDrawer(canvas);
 
+let kA = 0.5
+let kS = 0.5
+let kD = 0.5
+let n = 2
+let sourceLocation = {
+    "x": -2.0,
+    "y": 3.0,
+    "z": -1.0
+}
+let baseColor = "yellow" //kolor podstawy
+let sideColor = "green" //kolor ściany bocznej
 
-
-drawer.drawCylinder();
+drawer.drawCylinder(kA, kS, kD, n, sourceLocation, baseColor, sideColor);
